d57334579c83813c9ec20f127a8b0ec4285d1601
Theo Morra <td512@td512.io>
2019-10-25 06:52:02 +1300

    Fixes UI! Startup is now immediate

--- Humble Trove Downloader/Form1.vb
+++ Humble Trove Downloader/Form1.vb
@@ -8,96 +8,6 @@ Imports Syroot.Windows.IO
 Imports Microsoft.Win32
 Public Class Form1
 
-#Region "IE Fix"
-    Private Shared Sub SetBrowserFeatureControlKey(feature As String, appName As String, value As UInteger)
-        Using key = Registry.CurrentUser.CreateSubKey([String].Concat("Software\Microsoft\Internet Explorer\Main\FeatureControl\", feature), RegistryKeyPermissionCheck.ReadWriteSubTree)
-            key.SetValue(appName, DirectCast(value, UInt32), RegistryValueKind.DWord)
-        End Using
-    End Sub
-
-    Private Sub SetBrowserFeatureControl()
-        ' http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
-
-        ' FeatureControl settings are per-process
-        Dim fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName)
-
-        ' make the control is not running inside Visual Studio Designer
-        If [String].Compare(fileName, "devenv.exe", True) = 0 OrElse [String].Compare(fileName, "XDesProc.exe", True) = 0 Then
-            Return
-        End If
-
-        SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, GetBrowserEmulationMode())
-        ' Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
-        SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1)
-        SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0)
-        SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1)
-    End Sub
-
-    Private Function GetBrowserEmulationMode() As UInt32
-        Dim browserVersion As Integer = 7
-        Using ieKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Internet Explorer", RegistryKeyPermissionCheck.ReadSubTree, System.Security.AccessControl.RegistryRights.QueryValues)
-            Dim version = ieKey.GetValue("svcVersion")
-            If Nothing = version Then
-                version = ieKey.GetValue("Version")
-                If Nothing = version Then
-                    Throw New ApplicationException("Microsoft Internet Explorer is required!")
-                End If
-            End If
-            Integer.TryParse(version.ToString().Split("."c)(0), browserVersion)
-        End Using
-
-        Dim mode As UInt32 = 10000
-        ' Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode. Default value for Internet Explorer 10.
-        Select Case browserVersion
-            Case 7
-                mode = 7000
-                ' Webpages containing standards-based !DOCTYPE directives are displayed in IE7 Standards mode. Default value for applications hosting the WebBrowser Control.
-                Exit Select
-            Case 8
-                mode = 8000
-                ' Webpages containing standards-based !DOCTYPE directives are displayed in IE8 mode. Default value for Internet Explorer 8
-                Exit Select
-            Case 9
-                mode = 9000
-                ' Internet Explorer 9. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
-                Exit Select
-            Case 10
-                mode = 10000
-                ' Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
-                Exit Select
-            Case 11
-                mode = 11001
-                ' Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
-                Exit Select
-            Case Else
-                ' use IE10 mode by default
-                Exit Select
-        End Select
-
-        Return mode
-    End Function
-#End Region
-
     Public baseuri = "https://www.humblebundle.com/api/v1/trove/chunk?index="
     Public counter = 0
     Public chunked = False
@@ -117,70 +27,78 @@ Public Class Form1
     Public this_filepath = ""
     Public wc As New WebClient
     Public wb
+    Public exit_do = 0
 
-    Private Sub Form1_VisibleChanged(sender As Object, e As EventArgs) Handles MyBase.VisibleChanged
-        If Me.Visible Then
-            RichTextBox1.AppendText("Fetching Humble Trove Items")
-
-            Do
-
-                Dim client As New WebClient
-                Dim result As String = client.DownloadString(New Uri(baseuri + counter.ToString))
-                client.Dispose()
-
-                If result = "[]" Then
-                    Exit Do
-                Else
-                    Dim trove = JArray.Parse(result)
-                    For Each Row In trove
-                        If Row("downloads")("windows") IsNot Nothing Then
-                            win_downloads.Add($"https://www.humblebundle.com/api/v1/user/download/sign?filename={Row("downloads")("windows")("url")("web")}&machine_name={Row("downloads")("windows")("machine_name")}")
-                        End If
-                        If Row("downloads")("mac") IsNot Nothing Then
-                            mac_downloads.Add($"https://www.humblebundle.com/api/v1/user/download/sign?filename={Row("downloads")("mac")("url")("web")}&machine_name={Row("downloads")("mac")("machine_name")}")
-                        End If
-                        If Row("downloads")("linux") IsNot Nothing Then
-                            lin_downloads.Add($"https://www.humblebundle.com/api/v1/user/download/sign?filename={Row("downloads")("linux")("url")("web")}&machine_name={Row("downloads")("linux")("machine_name")}")
-                        End If
-                    Next
-                    counter += 1
-                End If
-            Loop
-            win_links = win_downloads.Count
-            mac_links = mac_downloads.Count
-            lin_links = lin_downloads.Count
-            all_links = win_links + mac_links + lin_links
-            RichTextBox1.AppendText(Environment.NewLine)
-            RichTextBox1.AppendText("Fetched Chunks! Parsing...")
-            RichTextBox1.AppendText(Environment.NewLine)
-            RichTextBox1.AppendText($"Windows: {win_links} available downloads")
-            RichTextBox1.AppendText(Environment.NewLine)
-            RichTextBox1.AppendText($"Mac: {mac_links} available downloads")
-            RichTextBox1.AppendText(Environment.NewLine)
-            RichTextBox1.AppendText($"Linux: {lin_links} available downloads")
-            RichTextBox1.AppendText(Environment.NewLine)
-            RichTextBox1.AppendText($"Total: {all_links} available downloads")
-
-            Label1.Visible = False
-            Label2.Visible = True
-            Label3.Visible = True
-            Label4.Visible = True
-            RadioButton1.Visible = True
-            RadioButton2.Visible = True
-            RadioButton3.Visible = True
-            RadioButton4.Visible = True
-            RadioButton5.Visible = True
-            RadioButton6.Visible = True
-            TextBox1.Visible = True
-            Button1.Visible = True
-            Button2.Visible = True
-            Button3.Visible = True
-            Label3.Visible = True
-            ProgressBar1.Visible = False
-            GroupBox1.Visible = True
-            GroupBox2.Visible = True
-            Label3.Text = save_to
+    Public Function ChunkDownloader(sender As Object, e As DownloadStringCompletedEventArgs)
+        If e.Result = "[]" Then
+            exit_do = 1
+        Else
+            counter += 1
         End If
+        Dim trove = JArray.Parse(e.Result)
+        For Each Row In trove
+            If Row("downloads")("windows") IsNot Nothing Then
+                win_downloads.Add($"https://www.humblebundle.com/api/v1/user/download/sign?filename={Row("downloads")("windows")("url")("web")}&machine_name={Row("downloads")("windows")("machine_name")}")
+            End If
+            If Row("downloads")("mac") IsNot Nothing Then
+                mac_downloads.Add($"https://www.humblebundle.com/api/v1/user/download/sign?filename={Row("downloads")("mac")("url")("web")}&machine_name={Row("downloads")("mac")("machine_name")}")
+            End If
+            If Row("downloads")("linux") IsNot Nothing Then
+                lin_downloads.Add($"https://www.humblebundle.com/api/v1/user/download/sign?filename={Row("downloads")("linux")("url")("web")}&machine_name={Row("downloads")("linux")("machine_name")}")
+            End If
+        Next
+    End Function
+    Public Async Function InitialLoad() As Task
+        Do
+            If exit_do = 1 Then
+                Exit Do
+            End If
+
+            Dim client As New WebClient
+            AddHandler client.DownloadProgressChanged, AddressOf DownloadProgressChanged
+            AddHandler client.DownloadStringCompleted, AddressOf ChunkDownloader
+            Await client.DownloadStringTaskAsync(New Uri(baseuri + counter.ToString))
+
+        Loop
+    End Function
+    Private Async Sub Form1_VisibleChanged(sender As Object, e As EventArgs) Handles MyBase.VisibleChanged
+        RichTextBox1.AppendText("Fetching Humble Trove Items")
+
+        Await Task.Run(Function() InitialLoad())
+        win_links = win_downloads.Count
+        mac_links = mac_downloads.Count
+        lin_links = lin_downloads.Count
+        all_links = win_links + mac_links + lin_links
+        RichTextBox1.AppendText(Environment.NewLine)
+        RichTextBox1.AppendText("Fetched Chunks! Parsing...")
+        RichTextBox1.AppendText(Environment.NewLine)
+        RichTextBox1.AppendText($"Windows: {win_links} available downloads")
+        RichTextBox1.AppendText(Environment.NewLine)
+        RichTextBox1.AppendText($"Mac: {mac_links} available downloads")
+        RichTextBox1.AppendText(Environment.NewLine)
+        RichTextBox1.AppendText($"Linux: {lin_links} available downloads")
+        RichTextBox1.AppendText(Environment.NewLine)
+        RichTextBox1.AppendText($"Total: {all_links} available downloads")
+
+        Label1.Visible = False
+        Label2.Visible = True
+        Label3.Visible = True
+        Label4.Visible = True
+        RadioButton1.Visible = True
+        RadioButton2.Visible = True
+        RadioButton3.Visible = True
+        RadioButton4.Visible = True
+        RadioButton5.Visible = True
+        RadioButton6.Visible = True
+        TextBox1.Visible = True
+        Button1.Visible = True
+        Button2.Visible = True
+        Button3.Visible = True
+        Label3.Visible = True
+        ProgressBar1.Visible = False
+        GroupBox1.Visible = True
+        GroupBox2.Visible = True
+        Label3.Text = save_to
     End Sub
 
     Public Sub Invoke(ByVal control As Control, ByVal action As Action)
@@ -441,7 +359,7 @@ Public Class Form1
     End Sub
 
     Public Sub DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
-        ProgressBar1.Value = e.ProgressPercentage
+        ProgressBar1.Invoke(Sub() ProgressBar1.Value = e.ProgressPercentage)
     End Sub
     Public Sub DownloadProgressCompleted(sender As Object, e As EventArgs)
 
@@ -527,4 +445,94 @@ Public Class Form1
         TextBox1.Text = sessionCookie
         TextBox1.Enabled = False
     End Sub
+
+#Region "IE Fix"
+    Private Shared Sub SetBrowserFeatureControlKey(feature As String, appName As String, value As UInteger)
+        Using key = Registry.CurrentUser.CreateSubKey([String].Concat("Software\Microsoft\Internet Explorer\Main\FeatureControl\", feature), RegistryKeyPermissionCheck.ReadWriteSubTree)
+            key.SetValue(appName, DirectCast(value, UInt32), RegistryValueKind.DWord)
+        End Using
+    End Sub
+
+    Private Sub SetBrowserFeatureControl()
+        ' http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
+
+        ' FeatureControl settings are per-process
+        Dim fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName)
+
+        ' make the control is not running inside Visual Studio Designer
+        If [String].Compare(fileName, "devenv.exe", True) = 0 OrElse [String].Compare(fileName, "XDesProc.exe", True) = 0 Then
+            Return
+        End If
+
+        SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, GetBrowserEmulationMode())
+        ' Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
+        SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1)
+        SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0)
+        SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1)
+    End Sub
+
+    Private Function GetBrowserEmulationMode() As UInt32
+        Dim browserVersion As Integer = 7
+        Using ieKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Internet Explorer", RegistryKeyPermissionCheck.ReadSubTree, System.Security.AccessControl.RegistryRights.QueryValues)
+            Dim version = ieKey.GetValue("svcVersion")
+            If Nothing = version Then
+                version = ieKey.GetValue("Version")
+                If Nothing = version Then
+                    Throw New ApplicationException("Microsoft Internet Explorer is required!")
+                End If
+            End If
+            Integer.TryParse(version.ToString().Split("."c)(0), browserVersion)
+        End Using
+
+        Dim mode As UInt32 = 10000
+        ' Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode. Default value for Internet Explorer 10.
+        Select Case browserVersion
+            Case 7
+                mode = 7000
+                ' Webpages containing standards-based !DOCTYPE directives are displayed in IE7 Standards mode. Default value for applications hosting the WebBrowser Control.
+                Exit Select
+            Case 8
+                mode = 8000
+                ' Webpages containing standards-based !DOCTYPE directives are displayed in IE8 mode. Default value for Internet Explorer 8
+                Exit Select
+            Case 9
+                mode = 9000
+                ' Internet Explorer 9. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
+                Exit Select
+            Case 10
+                mode = 10000
+                ' Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
+                Exit Select
+            Case 11
+                mode = 11001
+                ' Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
+                Exit Select
+            Case Else
+                ' use IE10 mode by default
+                Exit Select
+        End Select
+
+        Return mode
+    End Function
+#End Region
 End Class
